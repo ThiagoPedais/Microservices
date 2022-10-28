04ca1bf8b815e023764e4c4a8fd21322a0e5dec4
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-10-28 16:13:12 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/dev/hrpayroll/entities/Payment.java
@@ -0,0 +1,22 @@
+package com.dev.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Payment implements Serializable {
+
+
+    private String name;
+    private Double dailyIncome;
+    private Integer days;
+
+    public double getTotal() {
+        return days * dailyIncome;
+    }
+}
