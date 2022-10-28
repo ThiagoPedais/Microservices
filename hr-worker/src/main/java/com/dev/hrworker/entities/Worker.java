f8b1e3289758c2c1306292652bee9d68e2d48049
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-10-28 13:28:45 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/dev/hrworker/entities/Worker.java
@@ -0,0 +1,24 @@
+package com.dev.hrworker.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Double dailyIncome;
+}
